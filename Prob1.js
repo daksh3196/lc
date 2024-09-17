6d60bd2bc08efc6b2a282e8f7d3fb99fcf92a446
Daksh Taneja <daksh.taneja.3196@gmail.com>
2024-09-17 14:56:09 +0530

    first problem and updated Md docs

---
+++ Prob1.js
@@ -0,0 +1,91 @@
+class Node{
+	constructor(data){
+  	this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList{
+	constructor(){
+  	this.head = null;
+  }
+  
+  _pvt(){}
+  
+  append(data){
+  	const newNode = new Node(data);
+    if(!this.head){
+    	this.head = newNode;
+    } else{
+    	let current = this.head;
+      while(current.next !== null){
+				current = current.next;      
+      }
+      current.next = newNode;
+    }
+  }
+
+  delete(data){ 
+		if(!this.head){
+    	console.log("List is empty")
+    	return;
+    } else{
+    	let current = this.head;
+      let prevNode = null;
+    	while(current.data !== data){
+      	prevNode = current;
+      	current = current.next; 
+      }
+      console.log(prevNode, current);
+      prevNode.next = current.next;
+    }
+  }
+  
+  prepend(data){
+  	let newNode = new Node(data);
+    newNode.next = this.head;
+		this.head = newNode;
+  }
+  
+  search(data){
+		let current = this.head;
+    while (current !== null) {
+      if (current.data === data) {
+        return current;
+      }
+      current = current.next;
+    }
+    console.log("Item not found in the list or the list is empty!")        
+    return null;
+  }
+
+	print(){
+  	let current = this.head;
+    if(current !== null){
+    let elems = [];
+    	while(current.next !== null){
+      	/* console.log("Node Data: ", current.data, "  -->  ") */
+        elems.push(current.data)
+        current = current.next;
+      }
+      elems.push(current.data)
+      console.log([...elems])
+    } else{
+      console.log("List is empty!")        
+      return;
+    }
+  }
+
+}
+
+
+const linkedList = new LinkedList();
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+linkedList.prepend(5);
+linkedList.print(); 
+linkedList.delete(20);
+linkedList.print(); 
+console.log(linkedList.search(10));
+console.log(linkedList.search(50));
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# lc
+# LeetCode
+
+Writing down my Leetcode daily problems and solutions
+
+- Problem 1: Implement a Linked list with capability to append, delete, prepend, serch a node and print the list.
+- Problem 2:  
